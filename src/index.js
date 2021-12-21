882de15e0a2b13884beae9123104093a04bcdf4e
rickyalmeida <ricky.almeida@rxpro.com.br>
2021-12-21 19:11:25 -0300

    refactor: 💡 use params instead of query

--- src/index.js
+++ src/index.js
@@ -10,12 +10,12 @@ app.get('/', (request, response) => {
   return response.json({message: 'Warmed up'})
 })
 
-app.get('/cities', (request, response) => {
-  if (typeof request.query.name !== 'string') {
-    return response.status(400).send('Missing name query parameter')
+app.get('/cities/:name', (request, response) => {
+  if (typeof request.params.name !== 'string') {
+    return response.status(400).send('Missing name parameter')
   }
 
-  const name = normalize(request.query.name)
+  const name = normalize(request.params.name)
   const cities = list.filter(city => normalize(city.name).includes(name))
 
   return response.json(cities)
